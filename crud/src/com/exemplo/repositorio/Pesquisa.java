21234742eb2b3b1ff17e9840d30e59a8abd47651
guiguizan <guiguizanchettin@hotmail.com>
2021-02-03 17:43:47 -0300

    novas solicitacoes

---
+++ crud/src/com/exemplo/repositorio/Pesquisa.java
@@ -0,0 +1,14 @@
+package com.exemplo.repositorio;
+
+public class Pesquisa {
+	String pesquisado;
+
+	public String getPesquisado() {
+		return pesquisado;
+	}
+
+	public void setPesquisado(String pesquisado) {
+		this.pesquisado = pesquisado;
+	}
+	
+}
